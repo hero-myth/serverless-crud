2b8784140ec3bfbe35294636213839675fadfa04
hero-myth <jaimeagason8@gmail.com>
2025-11-23 12:46:35 -0500

    chore: switch from BrowserRouter to HashRouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import "./auth/bootstrap.js";
 import App from "./App.jsx";
@@ -18,9 +18,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<ThemeProvider theme={theme}>
 			<CssBaseline />
-			<BrowserRouter>
+			<HashRouter>
 				<App />
-			</BrowserRouter>
+			</HashRouter>
 		</ThemeProvider>
 	</React.StrictMode>
 );
